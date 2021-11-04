63b3a39cc306d8696c0493f9dbeb5ce4f3a5b998
Shafi Masoumi <masoumi2017@icould.com>
2021-11-04 10:07:11 -0700

    board test added

---
+++ tic-tac-toe/src/board.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Square from "./square";
+
+class Board extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
