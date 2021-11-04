c1444922fc65f669b1c75212c8ccf08dbeedd053
Shafi Masoumi <masoumi2017@icould.com>
2021-11-04 13:31:56 -0700

    working on App

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -32,12 +32,32 @@ class App extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
-      stepNumber: 0
+      stepNumber: 0,
       xIsNext: true
+    };
+  }
 
+  handleClick(i) {
+    const history = this.state.history.slice(0);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return
     }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
   }
   render() {
+    const history =  this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
     return (
       <>
       <h1>Shafi Tic Tac Toe Game</h1>
