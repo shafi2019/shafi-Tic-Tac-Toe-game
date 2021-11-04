75749a4a638e8211d4a2eff725298e0f450af374
Shafi Masoumi <masoumi2017@icould.com>
2021-11-04 13:54:35 -0700

    almost working

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -45,6 +45,15 @@ class App extends React.Component {
       return
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   jumpTo(step) {
@@ -58,15 +67,35 @@ class App extends React.Component {
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move # " + move : "Restart Game";
+      return (
+        <li key={move} className="moves-class">
+          <button></button>
+        </li>
+      )
+    })
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <>
       <h1>Shafi Tic Tac Toe Game</h1>
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
 
       </div>
